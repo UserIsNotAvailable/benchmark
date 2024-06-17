c6667cb74a84ee83dc6dfbeaf0041d864038c992
tiwang <tiwang@ncsoft.com>
2024-06-17 17:44:40 +0900

    Resolve subscription name conflict.

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/WorkloadGenerator.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/WorkloadGenerator.java
@@ -233,7 +233,7 @@ public class WorkloadGenerator implements AutoCloseable {
         for (String topic : topics) {
             for (int i = 0; i < workload.subscriptionsPerTopic; i++) {
                 String subscriptionName =
-                        String.format("sub-%03d-%s", i, RandomGenerator.getRandomString());
+                        String.format("sub-%03d-%s", i, RandomGenerator.getUuid());
                 for (int j = 0; j < workload.consumerPerSubscription; j++) {
                     consumerAssignment.topicsSubscriptions.add(
                             new TopicSubscription(topic, subscriptionName));
